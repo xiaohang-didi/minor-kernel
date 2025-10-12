afefc764d799732d66ced29e79262727fe8df2b3
liuhang <197@163.com>
2025-10-12 22:04:17 +0800

    改改名字

--- init/entry.c
+++ init/entry.c
@@ -1,21 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  entry.c
- *
- *    Description:  hurlex 内核的入口函数
- *
- *        Version:  1.0
- *        Created:  2013年10月31日 13时31分03秒
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Hurley (LiuHuan), liuhuan1992@gmail.com
- *        Company:  Class 1107 of Computer Science and Technology
- *
- * =====================================================================================
- */
-
 #include "gdt.h"
 #include "console.h"
 #include "debug.h"
