29238743e51274f1f3f923398f5c59827a2cd0a5
liuhang <197@163.com>
2025-10-12 13:48:50 +0800

    添加对gdt颗粒度的修改

--- gdt/gdt.c
+++ gdt/gdt.c
@@ -36,6 +36,8 @@ static void gdt_set_gate(int32_t num,uint32_t base,uint32_t limit,uint8_t access
     gdt_entries[num].base_high = (base >> 24) & 0xFF;
 
     gdt_entries[num].limit_low = (limit & 0xFFFF);
+    gdt_entries[num].granularity = (limit >> 16) & 0x0F;
+    
     gdt_entries[num].granularity |= gran & 0xF0;
     gdt_entries[num].access = access;
 }
